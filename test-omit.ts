751bb6e68b5e937b1e76267771f511de3f98b814
Duane Johnson <duane.johnson@gmail.com>
2026-06-14 16:11:31 -0600

    chore: clean up test files

--- test-omit.ts
+++
@@ -1,4 +0,0 @@
-type A = (() => string) & { a: number, b: string };
-type B = Omit<A, "a">;
-declare const b: B;
-b();
--- test-omit2.ts
+++
@@ -1,5 +0,0 @@
-type A = (() => string) & { a: number, b: string };
-type PreserveCall<T> = T extends (...args: infer A) => infer R ? (...args: A) => R : unknown;
-type B = Omit<A, "a"> & PreserveCall<A>;
-declare const b: B;
-b();
